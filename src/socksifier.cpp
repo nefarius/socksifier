2afe610fd9b41bd5a8d5f4df4c7aac9e54f81cbc
Benjamin Höglinger-Stelzer <benjamin.hoeglinger@nefarius.at>
2019-07-21 23:33:54 +0200

    Connecting to proxy works

--- src/socksifier.cpp
+++ src/socksifier.cpp
@@ -1,5 +1,6 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#include <MSWSock.h>
 #include <windows.h>
 #include <stdio.h>
 #include <string.h>
@@ -36,6 +37,8 @@ extern "C" {
 
     static int (WINAPI * real_connect)(SOCKET s, const struct sockaddr * name, int namelen) = connect;
 
+    LPFN_CONNECTEX LpfnConnectex = NULL;
+
 #ifdef __cplusplus
 }
 #endif
@@ -48,16 +51,98 @@ int WINAPI my_connect(SOCKET s, const struct sockaddr * name, int namelen)
 
     char addr[INET_ADDRSTRLEN];
     inet_ntop(AF_INET, &(dest->sin_addr), addr, INET_ADDRSTRLEN);
-    spdlog::info("Original connect destination: {}:{}", addr, ntohs(dest->sin_port));
+    const auto dest_port = ntohs(dest->sin_port);
+    spdlog::info("Original connect destination: {}:{}", addr, dest_port);
+
+    if (dest_port != 443)
+    {
+        return real_connect(s, name, namelen);
+    }
 
     struct sockaddr_in proxy;
     proxy.sin_addr.s_addr = settings.proxy_address;
     proxy.sin_family = AF_INET;
     proxy.sin_port = settings.proxy_port;
-    int ret = real_connect(s, (const struct sockaddr *)&proxy, sizeof(proxy));
-    if (ret) {
-        return ret;
+
+    inet_ntop(AF_INET, &(proxy.sin_addr), addr, INET_ADDRSTRLEN);
+    spdlog::info("Connecting to SOCKS proxy: {}:{}", addr, ntohs(proxy.sin_port));
+
+    DWORD numBytes = 0;
+    GUID guid = WSAID_CONNECTEX;
+
+    LPFN_CONNECTEX ConnectExPtr = NULL;
+    int success = WSAIoctl(
+        s,
+        SIO_GET_EXTENSION_FUNCTION_POINTER,
+        (void*)&guid,
+        sizeof(guid),
+        (void*)&ConnectExPtr,
+        sizeof(ConnectExPtr),
+        &numBytes,
+        NULL,
+        NULL
+    );
+    // Check WSAGetLastError()!
+
+    /* ... */
+
+
+    /* ConnectEx requires the socket to be initially bound. */
+    {
+        struct sockaddr_in addr;
+        ZeroMemory(&addr, sizeof(addr));
+        addr.sin_family = AF_INET;
+        addr.sin_addr.s_addr = INADDR_ANY;
+        addr.sin_port = 0;
+        auto rc = bind(s, (SOCKADDR*)&addr, sizeof(addr));
+        if (rc != 0) {
+            spdlog::error("bind failed: {}", WSAGetLastError());
+            return 1;
+        }
     }
+         
+    OVERLAPPED overlapped = { 0 };
+    overlapped.hEvent = CreateEvent(nullptr, FALSE, FALSE, nullptr);
+       
+    auto m = WSAGetLastError();
+
+    // Assuming the pointer isn't NULL, you can call it with the correct parameters.
+    auto retval = ConnectExPtr(
+        s,
+        reinterpret_cast<SOCKADDR *>(&proxy),
+        sizeof(proxy),
+        NULL,
+        0,
+        &numBytes, 
+        &overlapped
+    );
+
+    auto v = WSAGetLastError();
+
+    DWORD transfer = 0, flags = 0;
+
+    auto rv = WSAGetOverlappedResult(
+        s,
+        &overlapped,
+        &transfer,
+        TRUE,
+        &flags
+    );
+
+    //const int ret = real_connect(s, reinterpret_cast<SOCKADDR *>(&proxy), sizeof(proxy));
+    //if (ret) {
+    //    char *error = NULL;
+    //    FormatMessageA(
+    //        FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+    //        NULL,
+    //        WSAGetLastError(),
+    //        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+    //        (LPSTR)&error, 0, NULL);
+    //    spdlog::error("Proxy server connection failed: {} ({})", error, WSAGetLastError());
+    //    LocalFree(error);
+    //
+    //    return ret;
+    //}
 
     char buffer[256];
     buffer[0] = 0x04;
@@ -76,7 +161,7 @@ int WINAPI my_connect(SOCKET s, const struct sockaddr * name, int namelen)
         return SOCKET_ERROR;
     }
 
-    return ret;
+    return 0;
 }
 
 
